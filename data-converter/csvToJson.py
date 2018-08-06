8bd0291f57f0992629484f668f5de6865698e639
Davide Gaido <davide.0plus1@gmail.com>
2018-08-06 12:04:52 +1000

    Add missing quotes to a report resolves #3

--- data-converter/csvToJson.py
+++ data-converter/csvToJson.py
@@ -4,14 +4,17 @@
 
 import json
 import os.path
+from datetime import timedelta, datetime
 
 CSV_INPUT_FILE = './litclock_annotated.csv'
+MINUTES_IN_A_DAY = 60*24
 
 if not os.path.exists(CSV_INPUT_FILE):
     exit('{} is missing'.format(CSV_INPUT_FILE))
 
 csvfile = open(CSV_INPUT_FILE, 'r')
 jsonfile = open('./../src/quotes.json', 'w')
+reportfile = open('./../report.md', 'w')
 
 quotesDictionary={}
 
@@ -26,3 +29,20 @@ for line in csvfile:
     quotesDictionary[time].append(csv_line)
 
 json.dump(quotesDictionary, jsonfile, indent=2, sort_keys=True)
+
+# Let's parse and check if we have all the required times
+now = datetime(2018,1,1,0,0)
+end = now + timedelta(hours=24)
+missing_quotes = []
+while now <= end:
+    if now.strftime("%H:%M") not in quotesDictionary:
+        missing_quotes.append(now.strftime("%H:%M"))
+    now += timedelta(minutes=1)
+
+reportfile.write('# Missing quotes\n')
+reportfile.write('#### We are missing {} quotes: {}%\n\n'.format(len(missing_quotes), round(100 * float(len(missing_quotes))/float(MINUTES_IN_A_DAY), 2)))
+
+for missing_quote in missing_quotes:
+    reportfile.write('* {}\n'.format(missing_quote))
+
+exit()
